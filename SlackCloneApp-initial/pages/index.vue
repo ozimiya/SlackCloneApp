88823ff75ee67e5cae43524af220bcb5b082a44a
ozimiya <ozimiya2@gmail.com>
2021-02-03 17:24:13 +0900

    ログイン機能を実装

--- SlackCloneApp-initial/pages/index.vue
+++ SlackCloneApp-initial/pages/index.vue
@@ -1,37 +1,29 @@
 <template>
   <div class="container">
-	  <div class="chats-layout">
-			<Messages />
-	  </div>
-	  <div class="input-layout">
-			<ChatForm />
-	  </div>
+<!--	  表示できない..-->
+<!--	  <button>ログイン</button>-->
+<!--	  <button v-on:click="login">ログイン</button>-->
   </div>
 </template>
 
 <script>
-	import Messages from '~/components/Messages.vue'
-	import ChatForm from '~/components/ChatForm.vue'
+	import { firebase } from '~/plugins/firebase'
 
 	export default {
-		components: {
-			Messages,
-			ChatForm
+		methods: {
+			// login() {
+			// 	const provider = new firebase.auth.GoogleAuthProvider();
+			// 	firebase.auth().signInWithPopup(provider)
+			// 		.then((result) => {
+			// 			const user = result.user;
+			// 			console.log(user)
+			// 		}).catch((error) => {
+			// 		window.alert(error);
+			// 	})
+			// }
 		}
 	}
 </script>
 
 <style scoped>
-	.container {
-		height: 100%;
-	}
-
-	.chats-layout {
-		overflow: scroll;
-		height: 90%;
-	}
-
-	.input-layout {
-		height: 10%;
-	}
 </style>
